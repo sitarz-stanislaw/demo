ee777cd37fd2e221cfbe46f54ab2dd4ec46b6664
Stanislaw Sitarz <sitarzstanislaw@gmail.com>
2019-12-07 17:36:25 +0100

    initial commit

---
+++ src/test/java/pl/mruwa2000/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mruwa2000.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
